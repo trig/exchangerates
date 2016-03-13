19f2a22ea5c2910cbce6438a6defff56bde49899
Trig <trig.github@yandex.ru>
2016-03-13 23:29:40 +0300

    changed readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # exchangerates
-demo project
+demo project that simply fetches two currency exchange providers and display
+ that info on web page 
+
+I've picked Laravel PHP framework for backend and Twitter Bootstrap for frontend
+
+You can see live demo [here](http://178.62.176.120:8000/)
--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
-use function response;
 use function view;
 
 class LandingController extends BaseController {
