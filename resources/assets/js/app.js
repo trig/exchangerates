63f5f3998b2e52f31824f00ee0800c7d26cc862d
Trig <trig.github@yandex.ru>
2016-03-15 09:28:26 +0300

    added js handler

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,14 @@
-$(document).on('change', '.js_currency_code', function(){
-    console.info('will update DOM')
+$(document).on('change', '.js_currency_code', function () {
+    var provider = $(this).data('provider'),
+            code = $(this).val();
+    $.getJSON('/ajax/get_rates', {provider: provider}).done(function (d) {
+        if (undefined != d.payload[code]) {
+            console.log(code);
+            $('#' + provider + '_results').text(d.payload[code]);
+        } else {
+            console.warn('n/a');
+            $('#' + provider + '_results').text('n/a');
+        }
+    });
 })
 
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,27 +7,27 @@
     <div class="col-md-6">
         <form>
             <label>CBRF</label>
-            <select data-name="cbrf" class="js_currency_code form-control">
+            <select data-provider="cbrf" class="js_currency_code form-control">
                 @foreach($currency_codes as $name => $abbrev)
                 <option value="{{ $name }}"@if('USD' == $name)selected @endif>{{ $name }} {{ $abbrev }}</option>
                 @endforeach
             </select>
         </form>
-        <div id="cbrf_results" class="row currency_results">
-            
+        <div class="currency_results">
+             <h1 id="cbrf_results">n/a</h1>
         </div>
     </div>
      <div class="col-md-6">
         <form>
             <label>YAHOO</label>
-            <select data-name="yhoo" class="js_currency_code form-control">
+            <select data-provider="yhoo" class="js_currency_code form-control">
                 @foreach($currency_codes as $name => $abbrev)
                 <option value="{{ $name }}"@if('USD' == $name)selected @endif>{{ $name }} {{ $abbrev }}</option>
                 @endforeach
             </select>
         </form>
-          <div id="yhoo_results" class="row currency_results">
-            
+          <div class="currency_results">
+              <h1 id="yhoo_results">n/a</h1>
         </div>
     </div>
 </div>
